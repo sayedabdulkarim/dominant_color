298fad258ffe9dd0d7f2f27b000ea5ceef8311a4
unknown <sakarim9124@gmail.com>
2023-11-24 18:40:38 +0530

    updated

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+// import jwt from "jsonwebtoken";
+// import asyncHandler from "express-async-handler";
+// //parser
+// import UserModal from "../modals/userModal.js";
+
+// export const protectedRoutesWithParser = asyncHandler(
+//   async (req, res, next) => {
+//     const token = req.cookies.jwt; // JWT token from cookie
+//     const csrfToken = req.cookies["XSRF-TOKEN"]; // CSRF token from cookie
+//     const csrfTokenFromHeader = req.headers["x-csrf-token"]; // CSRF token from header
+
+//     // console.log({ token, csrfToken, csrfTokenFromHeader, req: req.headers });
+
+//     if (token && csrfToken && csrfTokenFromHeader) {
+//       try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//         // Verify CSRF token
+//         if (csrfToken !== csrfTokenFromHeader) {
+//           res.status(403);
+//           throw new Error("CSRF token validation failed");
+//         }
+
+//         // Token is valid, set the user in the request
+//         req.user = await UserModal.findById(decoded.id).select("-password");
+//         next();
+//       } catch (error) {
+//         console.error(error);
+//         res.status(401);
+//         throw new Error("Not authorized, token failed");
+//       }
+//     } else {
+//       res.status(401);
+//       throw new Error("Not authorized, token missing or CSRF token missing");
+//     }
+//   }
+// );
